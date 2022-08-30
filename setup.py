53f7053bf0457a787d23dacf5fc09decfb8e1cdf
Thomas Nipen <tnipen@gmail.com>
2022-08-30 13:07:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,9 +47,6 @@ setup(
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         'Topic :: Scientific/Engineering :: Information Analysis',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL License',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
